258aa64bef6b359855df4dcd530ec51baa9fa220
Waterbootdev <wassermann.sp@gmail.com>
2025-04-29 22:30:08 +0200

    CH2: Player L2: Game Loop

--- main.py
+++ main.py
@@ -4,13 +4,35 @@
 import pygame
 
 from constants import *
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, BLACK
+
+
 
 def main():
   
-    print(f"""Starting Asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}")""")
+    init_pygame()
+
+    screen = get_sreen()
+
+    while True :
+        if not handle_events():
+            return
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+
+def get_sreen():
+    return pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def init_pygame():
+    numpass, numfail = pygame.init()
+    print(f"pygame init : fail count : {numfail}, pass count : {numpass}")
 
 if __name__ == "__main__":
     main()
