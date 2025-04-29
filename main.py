8f08fa9e3e3694caee09a1a40a96d4a850bd02a7
Waterbootdev <wassermann.sp@gmail.com>
2025-04-29 08:22:58 +0200

    CH2:Player L1: Modules

---
+++ main.py
@@ -0,0 +1,18 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+  
+    print(f"""Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}")""")
+
+if __name__ == "__main__":
+    main()
+
+
